6099150bcb00fc2c2d8569cbf33437ab09c91960
Rubeel Husain Rizvi <65360353+rubeelhusain@users.noreply.github.com>
2022-01-31 23:48:11 +0530

    Add files via upload
    first commit

---
+++ day001project.py
@@ -0,0 +1,5 @@
+#band name generator
+print("Welcome to Rockban Name Generator")
+Hometown= input("Enter your Hometown\n").strip()
+Petname= input("Enter your pet's name\n").strip()
+print(Hometown + " " + Petname)
